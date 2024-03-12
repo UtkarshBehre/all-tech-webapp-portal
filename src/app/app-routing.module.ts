94b6259c89b86a17e7e32e11f9a92e4c878326db
Utkarsh Behre <utkarshbehre@microsoft.com>
2024-03-12 00:50:36 -0700

    refresh fix attempt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './dashboards/landing-page/landing-page.component';
 import { TodoListDashboardComponent } from './dashboards/todo-list-dashboard/todo-list-dashboard.component';
 import { PageNotFoundComponent } from './dashboards/page-not-found/page-not-found.component';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
@@ -13,6 +14,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{
+    provide: LocationStrategy, useClass: HashLocationStrategy
+  }]
 })
 export class AppRoutingModule { }
